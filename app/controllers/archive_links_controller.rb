8b4bb3041505c54af5ab964e138f11d06e8a495a
Ryan <ryan@ryanjm.com>
2012-12-03 18:13:53 -0700

    created partial

--- app/controllers/archive_links_controller.rb
+++ app/controllers/archive_links_controller.rb
@@ -3,13 +3,18 @@ class ArchiveLinksController < ApplicationController
 
   # Not handling any error cases here
   def create
-    link = WebLink.find(params[:web_link_id])
-    link.archive(@user)
+    @link = WebLink.find(params[:web_link_id])
+    @link.archive(@user)
 
-    if params[:archive_link]["redirect_to"] == 'index'
-      redirect_to web_links_path
-    else
-      redirect_to link
+    respond_to do |format|
+      format.html do
+        if params[:archive_link]["redirect_to"] == 'index'
+          redirect_to web_links_path
+        else
+          redirect_to link
+        end
+      end
+      format.js
     end
   end
 end
